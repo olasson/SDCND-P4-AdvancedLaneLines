aed93d2fc489833a2977e44fa0b8942e4e289b7e
olasson <oystein.lasson@gmail.com>
2021-05-24 17:25:41 +0200

    Added helper functions

---
+++ code/misc.py
@@ -0,0 +1,31 @@
+from os import mkdir, listdir
+from os.path import isdir as folder_exists
+from os.path import isfile, splitext
+
+def file_exists(path):
+
+    if path is None:
+        return False
+
+    if not isfile(path):
+        return False
+
+    return True
+
+def folder_guard(path):
+    if not folder_exists(path):
+        print('INFO:folder_guard(): Creating folder: ' + path + '...')
+        mkdir(path)
+
+def folder_is_empty(path):
+
+    if folder_exists(path):
+        return (len(listdir(path)) == 0)
+    
+    return True
+
+def remove_ext(file_name):
+
+    base_name = splitext(file_name)[0]
+
+    return base_name
