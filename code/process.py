b4f3c224a5301f454c7477ebd93aff83ffe14d1d
olasson <oystein.lasson@gmail.com>
2021-05-24 17:48:59 +0200

    Added pre_processing of image

---
+++ code/process.py
@@ -0,0 +1,172 @@
+import cv2
+import numpy as np
+
+from code.draw import draw_region
+from code.io import save_image
+
+SOBEL_KERNEL = 5
+IMAGE_MAX = 255
+
+# Internals
+
+def _threshold_binary(image, thresholds):
+
+    binary_out = np.zeros_like(image)
+
+    binary_out[(image >= thresholds[0]) & (image <= thresholds[1])] = 1
+
+    return binary_out
+
+def _threshold_binary_sobel_abs(sobel, thresholds):
+
+    sobel_tmp= np.absolute(sobel)
+
+    # Scale and convert to 'uint8'
+    sobel_abs = np.uint8((IMAGE_MAX * sobel_tmp) / np.max(sobel_tmp))
+
+    binary_out = _threshold_binary(sobel_abs, thresholds)
+
+    return binary_out
+
+def _threshold_binary_sobel_gradmag(sobel_x, sobel_y, thresholds):
+
+    sobel_tmp = np.sqrt((sobel_x * sobel_x) + (sobel_y * sobel_y))
+
+    # Scale and convert to 'uint8'
+    alpha = np.max(sobel_tmp) / IMAGE_MAX
+    sobel_gradmag = np.uint8(sobel_tmp / alpha)
+
+    binary_out = _threshold_binary(sobel_gradmag, thresholds)
+
+    return binary_out
+
+
+def _threshold_binary_sobel_dir(sobel_x, sobel_y, thresholds):
+
+    sobel_x_abs = np.absolute(sobel_x)
+    sobel_y_abs = np.absolute(sobel_y)
+
+    sobel_dir = np.arctan2(sobel_y_abs, sobel_x_abs)
+
+    binary_out = _threshold_binary(sobel_dir, thresholds)
+
+    return binary_out
+
+
+def _threshold_binary_hsv_channel(image, c, thresholds):
+
+    color_channel = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)[:, :, c]
+
+    binary_out = _threshold_binary(color_channel, thresholds)
+
+    return binary_out
+
+def _threshold_binary_hls_channel(image, c, thresholds):
+
+    color_channel = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)[:, :, c]
+
+    binary_out = _threshold_binary(color_channel, thresholds)
+
+    return binary_out
+
+def _threshold_binary_lab_channel(image, c, thresholds):
+
+    color_channel = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)[:, :, c]
+
+    binary_out = _threshold_binary(color_channel, thresholds)
+
+    return binary_out
+
+# Gradient
+
+def threshold_gradient(image):
+
+    gray_image  = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sobel_x = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize = SOBEL_KERNEL)
+    sobel_y = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize = SOBEL_KERNEL)
+
+    sobel_x_binary = _threshold_binary_sobel_abs(sobel_x, [15, IMAGE_MAX])
+    sobel_y_binary = _threshold_binary_sobel_abs(sobel_y, [25, IMAGE_MAX])
+
+    sobel_gradmag_binary = _threshold_binary_sobel_gradmag(sobel_x, sobel_y, [40, IMAGE_MAX])
+    sobel_dir_binary = _threshold_binary_sobel_dir(sobel_x, sobel_y, [0.7, 1.3])
+
+    v_channel_binary = _threshold_binary_hsv_channel(image, 2, [180, IMAGE_MAX])
+
+    gradient_binary = np.zeros_like(sobel_x_binary)
+
+    gradient_binary[(((sobel_x_binary == 1) & (sobel_y_binary == 1)) | (sobel_dir_binary == 1)) & (sobel_gradmag_binary == 1) & (v_channel_binary == 1)] = 1
+
+    return gradient_binary
+
+# Color
+
+def threshold_color(image):
+
+    r_channel_binary = _threshold_binary(image[:, :, 2], [195, IMAGE_MAX])
+    l_channel_binary = _threshold_binary_hls_channel(image, 1, [195, IMAGE_MAX])
+    s_channel_binary = _threshold_binary_hls_channel(image, 2, [100, IMAGE_MAX])
+    v_channel_binary = _threshold_binary_hsv_channel(image, 2, [140, IMAGE_MAX])
+    b_channel_binary = _threshold_binary_lab_channel(image, 2, [150, IMAGE_MAX])
+
+    color_binary = np.zeros_like(r_channel_binary)
+
+
+    color_binary[((b_channel_binary == 1) & (v_channel_binary == 1)) | ((r_channel_binary == 1) & (l_channel_binary == 1)) | ((s_channel_binary == 1) & (v_channel_binary == 1))] = 1
+
+    return color_binary
+
+def gamma_correction(image, gamma):
+
+    table = np.zeros(256)
+    for i in np.arange(0, 256):
+        table[i] = ((i / 255.0) ** gamma) * 255.0
+
+    gamma_image = cv2.LUT(image, table.astype('uint8'))
+
+    return gamma_image
+
+
+def warp_image(image, src, dst, n_rows, n_cols):
+
+    warp_matrix = cv2.getPerspectiveTransform(src, dst)
+
+    image_warped = cv2.warpPerspective(image, warp_matrix, (n_cols, n_rows))
+
+    return image_warped
+
+def unwarp_image(image, src, dst, n_rows, n_cols):
+
+    unwarped_image = warp_image(image, dst, src, n_rows, n_cols)
+
+    return unwarped_image
+
+
+def pre_process_image(image, mtx, dist, src, dst, n_rows, n_cols, debug_path = None):
+
+    image_undistorted = cv2.undistort(image, mtx, dist, None, mtx)
+
+    #image_gamma = gamma_correction(image_undistorted, 4.0)
+
+    gradient_binary = threshold_gradient(image_undistorted)
+    color_binary = threshold_color(image_undistorted)
+
+    combined_threshold = np.zeros_like(gradient_binary)
+    combined_threshold[(gradient_binary == 1) | (color_binary) == 1] = IMAGE_MAX
+
+    gray_warped = warp_image(combined_threshold, src, dst, n_rows, n_cols)
+
+    if debug_path is not None:
+
+        # Create a warped RGB image for src and dst
+        image_warped = warp_image(image, src, dst, n_rows, n_cols)
+
+        save_image(debug_path, 'step01_image_undistorted.png', image_undistorted)
+        #save_image(debug_path, 'step02_image_gamma.png', image_gamma)
+        save_image(debug_path, 'step03_gradient_binary.png', gradient_binary * IMAGE_MAX)
+        save_image(debug_path, 'step04_color_binary.png', color_binary * IMAGE_MAX)
+        save_image(debug_path, 'step05_combined_threshold.png', combined_threshold)
+        save_image(debug_path, 'step06_src.png', draw_region(image, src))
+        save_image(debug_path, 'step07_dst.png', draw_region(image_warped, dst))
+
+    return image_undistorted, gray_warped
